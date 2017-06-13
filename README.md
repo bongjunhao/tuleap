0130fd2a29f6d7d78f03bff61e0e9c487234921a
Bong Jun Hao <bongjunhao@gmail.com>
2017-06-13 14:55:04 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# tuleap
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: tuleap
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package_url = 'http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm'
+package_name = ::File.basename(package_url)
+package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+
+remote_file package_local_path do
+  source package_url
+end
+
+package package_name do
+  source package_local_path
+  provider Chef::Provider::Package::Rpm
+end
+
+template '/etc/yum.repos.d/Tuleap.repo' do
+  source 'Tuleap.repo.erb'
+end
+
+# execute 'install-tuleap' do
+#   command 'yum install -y tuleap-all tuleap-plugin-git-gitolite3'
+# end
